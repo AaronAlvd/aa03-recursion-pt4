b960135355fc895299d0d476ce0b5e11d91c0eba
Andrew <andrew.lizon12@gmail.com>
2024-04-13 13:00:19 -0700

    Solving Problem 15

--- problems/15-bonus-subsets.js
+++ problems/15-bonus-subsets.js
@@ -16,19 +16,47 @@ Hint: For subsets([1, 2, 3]), there are two kinds of subsets:
      subset that is the same, except it also does contain 3.
 ***********************************************************************/
 
+// const subsets = (arr) => {
+//   debugger
+//     if (arr.length === 0) {
+//       return [[]];
+//     } else {
+//      [[]].concat(arr)
+//      return subsets(arr.slice(1))
+//     }
+// }
+
+
+
+
+
 const subsets = (arr) => {
-  debugger
-    if (arr.length === 0) {
-      return [[]];
-    } else {
-     [[]].concat(arr)
-     return subsets(arr.slice(1))
-    }
+if () {
+
 }
 
+else {
+return subsets(arr)
+}
+
+}
+
+
+
+
+// let array = [[]];
+// for (let el of arr) {
+//   array.push([el])
+// }
+// console.log(array)
+
+
+
+
+
 // console.log(subsets([])) // [[]]
-console.log(subsets([1])) // [[], [1]]
-// subsets([1, 2]) // [[], [1], [2], [1, 2]]
+// console.log(subsets([1])) // [[], [1]]
+console.log(subsets([1, 2])) // [[], [1], [2], [1, 2]]
 // subsets([1, 2, 3]) // [[], [1], [2], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3]]
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
